f62ad42ec0b536418e25d3501854f8375120642a
Hamzath Anees <hamzath.anees@ooredoo.mv>
2020-03-14 16:04:07 +0500

    Latetst

--- js/custom.js
+++ js/custom.js
@@ -28,6 +28,14 @@
     }
   });
 
-  $('#datetimepicker1').datetimepicker();
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  var options={
+    format: 'dd/mm/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
 
 })(jQuery);
