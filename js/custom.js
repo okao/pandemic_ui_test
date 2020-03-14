4fe6ee4b169462b5f41f23858c3b073892fc651f
Hamzath Anees <hamzath.anees@ooredoo.mv>
2020-03-14 19:12:16 +0500

    Latetst

--- js/custom.js
+++ js/custom.js
@@ -38,4 +38,20 @@
   };
   date_input.datepicker(options);
 
+
+  $('.field').on('focus', function() {
+    $('body').addClass('is-focus');
+  });
+
+  $('.field').on('blur', function() {
+    $('body').removeClass('is-focus is-type');
+  });
+
+  $('.field').on('keydown', function(event) {
+    $('body').addClass('is-type');
+    if((event.which === 8) && $(this).val() === '') {
+      $('body').removeClass('is-type');
+    }
+  });
+
 })(jQuery);
